c3fd6cec13916c0182041aa3963ba2ca33b782e5
Julia Romanowski <juliaromanowski@gmail.com>
2019-04-08 19:50:45 -0400

    added text on calc

--- src/components/Calculator.vue
+++ src/components/Calculator.vue
@@ -1,6 +1,25 @@
 <template>
-  <div>
-    <h1>I am the calculator</h1>
+  <div class="calculator">
+    <div class="display">8008.88</div>
+    <div>C</div>
+    <div>+/-</div>
+    <div>%</div>
+    <div>÷</div>
+    <div>7</div>
+    <div>8</div>
+    <div>9</div>
+    <div>x</div>
+    <div>4</div>
+    <div>5</div>
+    <div>6</div>
+    <div>-</div>
+    <div>1</div>
+    <div>2</div>
+    <div>3</div>
+    <div>+</div>
+    <div class="zero">0</div>
+    <div>.</div>
+    <div>=</div>
   </div>
 </template>
 
@@ -8,5 +27,20 @@
 export default {};
 </script>
 
-<style>
+<style scoped>
+.calculator {
+  font-size: 40px;
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  grid-auto-rows: minmax(50px, auto);
+}
+
+.display {
+  grid-column: 1/5;
+  background-color: whitesmoke;
+}
+
+.zero {
+  grid-column: 1/3;
+}
 </style>
