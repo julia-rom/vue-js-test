e5b81ed36c4f26b5433045d8db6e8a96c944b677
Julia Romanowski <juliaromanowski@gmail.com>
2019-04-08 20:18:20 -0400

    added calculator colors + C button functionality

--- src/components/Calculator.vue
+++ src/components/Calculator.vue
@@ -1,34 +1,47 @@
 <template>
   <div class="calculator">
-    <div class="display">8008.88</div>
-    <div>C</div>
-    <div>+/-</div>
-    <div>%</div>
-    <div>÷</div>
-    <div>7</div>
-    <div>8</div>
-    <div>9</div>
-    <div>x</div>
-    <div>4</div>
-    <div>5</div>
-    <div>6</div>
-    <div>-</div>
-    <div>1</div>
-    <div>2</div>
-    <div>3</div>
-    <div>+</div>
-    <div class="zero">0</div>
-    <div>.</div>
-    <div>=</div>
+    <div class="display">{{current || '0'}}</div>
+    <div @click="clear" class="btn">C</div>
+    <div class="btn">+/-</div>
+    <div class="btn">%</div>
+    <div class="btn operator">÷</div>
+    <div class="btn">7</div>
+    <div class="btn">8</div>
+    <div class="btn">9</div>
+    <div class="btn operator">x</div>
+    <div class="btn">4</div>
+    <div class="btn">5</div>
+    <div class="btn">6</div>
+    <div class="btn operator">-</div>
+    <div class="btn">1</div>
+    <div class="btn">2</div>
+    <div class="btn">3</div>
+    <div class="btn operator">+</div>
+    <div class="btn zero">0</div>
+    <div class="btn">.</div>
+    <div class="btn operator">=</div>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      current: "123"
+    };
+  },
+  methods: {
+    clear() {
+      this.current = "";
+    }
+  }
+};
 </script>
 
 <style scoped>
 .calculator {
+  margin: 0 auto;
+  width: 400px;
   font-size: 40px;
   display: grid;
   grid-template-columns: repeat(4, 1fr);
@@ -37,10 +50,21 @@ export default {};
 
 .display {
   grid-column: 1/5;
-  background-color: whitesmoke;
+  background-color: black;
+  color: white;
 }
 
 .zero {
   grid-column: 1/3;
+}
+
+.btn {
+  border: 1px solid rgb(113, 110, 110);
+  background-color: white;
+}
+
+.operator {
+  background-color: orange;
+  color: white;
 }
 </style>
