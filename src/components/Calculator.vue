5cdbc33b4e843a05004d5b9adb386dcad1372f61
Julia Romanowski <juliaromanowski@gmail.com>
2019-04-08 20:24:21 -0400

    added sign method

--- src/components/Calculator.vue
+++ src/components/Calculator.vue
@@ -2,7 +2,7 @@
   <div class="calculator">
     <div class="display">{{current || '0'}}</div>
     <div @click="clear" class="btn">C</div>
-    <div class="btn">+/-</div>
+    <div @click="sign" class="btn">+/-</div>
     <div class="btn">%</div>
     <div class="btn operator">÷</div>
     <div class="btn">7</div>
@@ -33,6 +33,12 @@ export default {
   methods: {
     clear() {
       this.current = "";
+    },
+    sign() {
+      this.current =
+        this.current.charAt(0) === "-"
+          ? this.current.slice(1)
+          : `-${this.current}`;
     }
   }
 };
