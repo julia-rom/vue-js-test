e5e14217c133fa06b7b4ff4d411a72cc04fa373a
Julia Romanowski <juliaromanowski@gmail.com>
2019-04-10 11:32:45 -0400

    added few more operators

--- src/components/Calculator.vue
+++ src/components/Calculator.vue
@@ -3,22 +3,22 @@
     <div class="display">{{current || '0'}}</div>
     <div @click="clear" class="btn">C</div>
     <div @click="sign" class="btn">+/-</div>
-    <div class="btn">%</div>
-    <div class="btn operator">÷</div>
-    <div class="btn">7</div>
-    <div class="btn">8</div>
-    <div class="btn">9</div>
+    <div @click="percent" class="btn">%</div>
+    <div @click="divide" class="btn operator">÷</div>
+    <div @click="append('7')" class="btn">7</div>
+    <div @click="append('8')" class="btn">8</div>
+    <div @click="append('9')" class="btn">9</div>
     <div class="btn operator">x</div>
-    <div class="btn">4</div>
-    <div class="btn">5</div>
-    <div class="btn">6</div>
+    <div @click="append('4')" class="btn">4</div>
+    <div @click="append('5')" class="btn">5</div>
+    <div @click="append('6')" class="btn">6</div>
     <div class="btn operator">-</div>
-    <div class="btn">1</div>
-    <div class="btn">2</div>
-    <div class="btn">3</div>
+    <div @click="append('1')" class="btn">1</div>
+    <div @click="append('2')" class="btn">2</div>
+    <div @click="append('3')" class="btn">3</div>
     <div class="btn operator">+</div>
-    <div class="btn zero">0</div>
-    <div class="btn">.</div>
+    <div @click="append('0')" class="btn zero">0</div>
+    <div @click="dot" class="btn">.</div>
     <div class="btn operator">=</div>
   </div>
 </template>
@@ -27,7 +27,10 @@
 export default {
   data() {
     return {
-      current: "123"
+      current: "123",
+      operator: null,
+      operatorClicked: false,
+      previous: null
     };
   },
   methods: {
@@ -39,6 +42,23 @@ export default {
         this.current.charAt(0) === "-"
           ? this.current.slice(1)
           : `-${this.current}`;
+    },
+    percent() {
+      this.current = `${parseFloat(this.current) / 100}`;
+    },
+    // Clear number if operator is clicked, otherwise
+    // add number to the end of current
+    append(number) {
+      if (this.operatorClicked) {
+        this.current = "";
+        this.operatorClicked = false;
+      }
+      this.current = `${this.current}${number}`;
+    },
+    dot() {
+      if (this.current.indexOf(".") === -1) {
+        this.append(".");
+      }
     }
   }
 };
